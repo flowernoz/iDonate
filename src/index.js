43ffa2e8443d4e5ca0f4b46314f8df61cfb95d5e
Flowernoza <cadillac2111@Gulnozas-Mac.local>
2024-03-21 09:44:32 +0500

    files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { MyBooleanProvider } from "./context";
-import "./index.css";
-import "./media.css";
+import './styles/global.css';
+import "./styles/media.css";
 
 const root = createRoot(document.getElementById("root"));
 
